b6e995af5e598ac38629d376fef996737de1488a
xypnox <xypnox@gmail.com>
2017-12-11 17:48:23 +0530

    Added Registration section and Tabbed schedule

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GMUNC IIT KGP
 
 This is a official website of GMUNC IITKGP
+
+It can be viewed at gmunc-iitkgp.github.io/website
--- js/main.js
+++ js/main.js
@@ -14,4 +14,25 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+function openDay(evt, dayName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(dayName).style.display = "block";
+    evt.currentTarget.className += " active";
 } 
