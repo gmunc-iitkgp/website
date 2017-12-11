38d3d3e7f9d9d477d66fc2fb49c9eee0b5a28f92
xypnox <xypnox@gmail.com>
2017-12-11 21:30:52 +0530

    Minimized CSS and JS also adjusted a little font

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
-
-// Get the navbar
 var navbar = document.getElementById("menu");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -15,35 +8,22 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-
 function openDay(evt, dayName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(dayName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-
-/* Open when someone clicks on the span element */
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
-} 
+}
