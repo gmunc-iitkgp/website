ce5f634dc8db4773523e0a5a758291bb2df8aa31
xypnox <xypnox@gmail.com>
2017-12-13 00:13:22 +0530

    Added the accordion

--- js/main.js
+++ js/main.js
@@ -27,3 +27,16 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
