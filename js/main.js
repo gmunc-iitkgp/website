19e7f9d915f12193050ea0e5b4b2c875521a9da3
Aditya Vikram Singh <xypnox@gmail.com>
2017-12-19 22:33:44 +0530

    Added icons, improved footer, designed better announcements

--- js/main.js
+++ js/main.js
@@ -27,16 +27,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  }
-}
