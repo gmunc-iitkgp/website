1d83d5c4e2fda0c3439fadbe62dfc09c9bc4598f
xypnox <xypnox@gmail.com>
2018-01-16 23:08:42 +0530

    Smoother animations... SMOOOOOOOTH :wink:

--- js/anim-main.js
+++ js/anim-main.js
@@ -1,7 +1,10 @@
-window.sr = ScrollReveal({ duration: 1000 });
+window.sr = ScrollReveal({  duration: 1000,
+                            scale: 0.8,
+                            viewFactor: 0.4
+                        });
 sr.reveal('.header');
-sr.reveal('.icon', {delay: 500});
-sr.reveal('.menu', {delay: 1000});
+sr.reveal('.icon', {delay: 400});
+sr.reveal('.menu', {delay: 650});
 sr.reveal('.intro');
 sr.reveal('.announcement');
 sr.reveal('.image-box');
