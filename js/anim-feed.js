7409a98d7b27bae7433d852d2eff2bfdf966eb07
xypnox <xypnox@gmail.com>
2018-02-08 19:04:28 +0530

    Added feed animations

---
+++ js/anim-feed.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ duration: 1000 });
+sr.reveal('.header');
+sr.reveal('.icon', {delay: 200});
+sr.reveal('.panel');
+sr.reveal('.card', {duration: 1500, viewFactor: 0.4});
+sr.reveal('.article', {duration: 1000});
