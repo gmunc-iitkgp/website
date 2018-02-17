4d01411e354a1928ed87026c45dabc2365debdfb
xypnox <xypnox@gmail.com>
2018-02-18 01:36:23 +0530

    improved Mobile :iphone: feed :newspaper:

--- js/anim-feed.js
+++ js/anim-feed.js
@@ -2,5 +2,5 @@ window.sr = ScrollReveal({ duration: 1000 });
 sr.reveal('.header');
 sr.reveal('.icon', {delay: 200});
 sr.reveal('.panel');
-sr.reveal('.card', {duration: 1500, viewFactor: 0.4});
+sr.reveal('.card', {duration: 1500, viewFactor: 0.3});
 sr.reveal('.article', {duration: 1000});
